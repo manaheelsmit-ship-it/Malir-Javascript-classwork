05e3719dd299deb46cbbd792830cd390c159828b
Arham <arhamali63@gmail.com>
2026-03-03 11:08:51 +0500

    first commit

---
+++ chapter1.js
@@ -0,0 +1,6 @@
+// question 1
+
+alert("hello ")
+
+// question 2 
+alert("")
---
+++ script.js
@@ -0,0 +1,250 @@
+// // // // console.log("moattar ")
+// // // // // alert("welcome to my class")
+
+// // // // var name = "manaheel"
+// // // // var username = "fabiha"
+// // // // console.log(username)
+
+// // // // var lastname;  // declaration
+// // // // console.log(lastname) //undefined
+
+// // // // lastname = "akmal" //initialization
+// // // // console.log(lastname) //akmal
+
+// // // // username = "john"
+// // // // console.log(username)
+
+// // // // alert(username)
+
+
+// // // // var my_var = 43;
+// // // // var num2 = 40
+// // // // var num3 = 20
+// // // // var Total = num3 - num2
+// // // // var userName = "manaheel"
+// // // // console.log(Total)
+
+// // // // student name === bablu
+// // // // class === 5
+// // // // mathsMarks = 50/100
+// // // // english == 10/100
+// // // // urdu == 100/100 
+
+
+// // // // var mathsMarks = 50
+// // // // var englishMarks = 10
+// // // // var urduMarks = 100
+// // // // var total = 300
+// // // // var obtainedMarks = mathsMarks +englishMarks+ urduMarks
+// // // // var percentage =(obtainedMarks / total) * 100
+// // // // console.log(percentage)
+
+// // // // var userName = "sparrow"
+// // // // alert("2"+userName)
+// // // // var userName = prompt("What is your name ???")
+// // // // var age = prompt("bhai kitny saal ky ho ???")
+// // // // var grade = prompt("bhai knsi class my ho ???")
+// // // // alert("Welcome " + userName + " in my class"+ "bhai ap " + age + " saal ky ho "+ "bhai ap " +grade+ " class my ho ")
+// // // // console.log(userName + age + grade)
+
+
+
+// // // // var userData = prompt("whats your age ????")
+// // // // if (userData >= 18){
+// // // //     console.log("welcome to my site you are allowed")
+// // // // } else{
+// // // //     console.log("you are not allowed")
+// // // // }
+
+// // // // = // for assigning values
+// // // // == // just compare  values
+// // // // === // compare values and type both
+// // // // !== //
+// // // // !=
+// // // // >
+// // // // <
+// // // // >=
+// // // // =<
+
+// // // // var age = prompt("what's your AGE") //20
+// // // // var clas = prompt("what's your class") //8
+// // // // var grade = prompt("what your grade")
+// // // // // console.log(typeof(grade))
+// // // // if (age > 18 || clas === 10 || grade == "A"){
+// // // //     alert("allowed")
+// // // // }
+// // // // else{
+// // // //     alert("not allowed")
+// // // // }
+
+// // // // var username1 = "sana"
+// // // // var username2 = "huma"
+// // // // // console.log(username1)
+
+// // // // var studentName = ["sana","huma", "bisma"]
+// // // // console.log(studentName, "raw array")
+
+// // // // studentName.pop()
+// // // // console.log(studentName, "remove element in the last ")
+
+// // // // studentName.push("zara","aisha","ali")
+// // // // console.log(studentName, "add three element in the last ")
+
+// // // // studentName.shift()
+// // // // console.log(studentName, "remove one element from the begining ")
+
+// // // // studentName.unshift("rumesa", "liha")
+// // // // console.log(studentName, "add two element from the begining ")
+
+// // // // studentName.splice(4,1, "nimra")
+// // // // console.log(studentName)
+
+// // // // studentName.splice(5,0,"shakeel bhai")
+// // // // console.log(studentName)
+
+// // // // studentName.splice(3,2)
+// // // // console.log(studentName)
+
+// // // // studentName.splice(1,3,"manahil", "hania", "gafoor bhai")
+// // // // console.log(studentName)
+// // // // var newarray = studentName.slice(1,3)
+// // // // console.log(newarray)
+
+
+// // // // var num = 2
+// // // // num++
+// // // // console.log(num)
+
+
+// // // // if (countryName[0] === "Saudia") {
+// // // //     console.log("i found it ")
+// // // // } else if (countryName[1] === "Saudia") {
+// // // //     console.log("i found it ")
+// // // // } else if (countryName[2] === "Saudia") {
+// // // //     console.log("i found it ")
+// // // // }
+// // // // else if (countryName[3] === "Saudia") {
+// // // //     console.log("i found it ")
+// // // // }
+// // // // else if (countryName[4] === "Saudia") {
+// // // //     console.log("i found it ")
+// // // // }
+// // // // else {
+// // // //     console.log("i didn't found")
+// // // // }
+
+
+
+// // // // var countryName = ["Pakistan", "Iran", "Saudia", "dubai", "Turkey"]
+
+// // // // for (var i = 0; i<= 4; i++){
+// // // // console.log(countryName[i])
+// // // // }
+
+
+// // // // let a = 8
+// // // // let b = 5
+// // // // console.log(a++) // 8
+// // // // console.log(--b) // 4
+// // // // console.log(--a) // 8
+// // // // console.log(++a) // 9 
+// // // // a = b
+// // // // console.log(a++) // 9 
+// // // // console.log(b++) //  9
+// // // // console.log(a,b) // 10,10
+// // // // var countryname = ["india", "pakistan", "turkey", "china", "iraq"]
+// // // // console.log(countryname.length)
+// // // // for (var i = 0; i <= countryname.length; i++) {
+// // // //    if (countryname[i] === "pakistan"){
+// // // //     console.log("found it!!!! ")
+// // // //     break;
+// // // //    }else{
+// // // //     console.log("not found ")
+// // // //    }
+// // // // }
+// // // // var userName = ["salman", "sharukh", "Rehman", "imran", "altaf"]
+// // // // var lastName = ["khan", "khan", "dakait", "khan", "bhai"]
+// // // // var fullName = []
+// // // // for (var i = 0 ; i<userName.length ; i++){
+// // // //     fullName.push(userName[i])
+
+// // // // }
+// // // // for (var j = 0 ; j<lastName.length ; j++){
+// // // //     fullName.push(lastName[j])
+// // // // }
+// // // // console.log(fullName)
+
+
+// // // // var userData = "manahil"
+// // // // var data = prompt("what is your name ")
+// // // // var updatedData = data.toLowerCase()
+// // // // if (updatedData === "manahil" || updatedData === "manaheel" || updatedData === "mana") {
+// // // //     alert("come in ")
+// // // // } else {
+// // // //     alert("not allowed ")
+// // // // }
+
+
+// // // // var array = ["red", "blue", "orange", "pink"]
+// // // // for (var i = 0; i < array.length; i++) {
+// // // //     console.log(array[i])
+// // // // }
+
+
+// // // function teaMaker(sugar, milk) {
+// // //     // tea + sugar + milk
+// // //     var tea = 1 + sugar + milk
+// // //     return tea
+// // // }
+
+// // // console.log(teaMaker(1, 8))
+// // // var result = teaMaker(2, 4)
+// // // console.log(result)
+// // // background: linear-gradient(180deg, #FF3B64 0%, #FFC371 100%);
+// // // var percentage = Number(prompt("what's your percentage "));
+// // // switch(true) {
+// // //     case percentage >= 80:
+// // //         alert("A+")
+// // //         break;
+// // //     case percentage >= 70:
+// // //         alert("A")
+// // //         break;
+// // //     case percentage >= 60:
+// // //         alert("B")
+// // //         break;
+// // //     case percentage >= 50:
+// // //         alert("C")
+// // //         break;
+// // //     default:
+// // //         alert("you are failed")
+// // // }
+
+// // var num = 7 
+// // function add(){
+// //     var sum = 5 
+// // }
+// // add()
+// // console.log(sum)
+
+
+// for (var i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+// var i = 0;
+// while (i < 10) {
+//     console.log(i)
+//     i++
+// }
+
+// var i =0;
+// do {
+//     console.log(i)
+//     i++;
+// }while(i < 0)
+
+function data(event) {
+        event.preventDefault(); 
+    var user = document.getElementById("inputEmail4").value
+    var password = document.getElementById("inputPassword4").value
+    console.log(user, password)
+}
